c388bc1da1a7112a72180335b44b0609aea0c073
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 12:47:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(katz_deli)
   if katz_deli.length > 0 
     katz_deli.each do |name, index|
      puts " The line is currently: #{index +1}. #{name}"
+   end
    else
      puts "The line is curretly empty."
-   end
  end
 end
 
