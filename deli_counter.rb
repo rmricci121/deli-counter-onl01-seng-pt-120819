81fd1e040492608fbda2f16ef3c9227b915ba35e
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 13:36:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,8 @@ def now_serving(katz_deli)
   if katz_deli == 0 
     puts "There is nobody waiting to be served"
   else 
-    puts "Currently serving #{katz_deli}."
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
   end
 end
   
