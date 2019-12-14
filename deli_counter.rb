e7ad7e4245609132dddc61c2f932aa1d08d5ce0c
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 12:48:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
      puts " The line is currently: #{index +1}. #{name}"
    end
    else
-     puts "The line is curretly empty."
+     puts "The line is currently empty."
  end
 end
 
