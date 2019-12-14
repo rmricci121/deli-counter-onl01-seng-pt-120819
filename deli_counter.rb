d9a0cfc749e99eb1aa1c5e4c4b77a924bdf4e145
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 12:57:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,12 @@ def line(katz_deli)
   phrase = "The Line is currently: "
   if katz_deli.length > 0 
     katz_deli.each_with_index do |name, index|
-     puts "The line is currently: #{index + 1}. #{name}"
+     phrase += "#{index + 1}. #{name}"
    end
    else
      puts "The line is currently empty."
  end
+ puts phrase
 end
 
 def take_a_number(katz_deli, name)
