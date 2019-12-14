f7647679183ee6b3c08236357881be5e4bf84617
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 12:58:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,7 @@ def line(katz_deli)
     katz_deli.each_with_index do |name, index|
      phrase += "#{index + 1}. #{name}"
    end
+   puts phrase
    else
      puts "The line is currently empty."
  end
