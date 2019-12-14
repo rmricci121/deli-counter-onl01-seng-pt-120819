118c92d6cece6530485597e76bfb7f1b78ec70a7
Robert M Ricci <rmricci121@gmail.com>
2019-12-14 12:45:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@ def line(katz_deli)
   katz_deli = []
   if katz_deli.length > 0 
     katz_deli.each do |name, index|
-      
+     puts " The line is currently: #{index}. #{name}"
+   end
+ end
 end
 
 def take_a_number(katz_deli, name)
